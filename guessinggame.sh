642a9411c77abe780213e4d8edb345fe53f947ad
Tenkorang17 <josephoforitenkorang390@gmail.com>
2022-07-24 19:00:22 +0000

    Updated the guessing game file

---
+++ guessinggame.sh
@@ -0,0 +1,25 @@
+
+function countFiles {
+	while true
+	do
+		echo "How many files are the current dirctory?: "
+        	read Files
+
+        	local number=$(pwd | ls | wc -l)
+
+		if [[ $Files -eq $number ]]
+		then
+		 	echo "Congratulations, you got it right"
+			break
+		else
+			if [[ $Files -gt $number ]]
+			then
+				echo "Your guessed number is larger than the expected number"
+			else
+				echo "Your guessed number is smaller than the \
+					expected number"
+			fi	
+		fi
+		
+	done
+}		
